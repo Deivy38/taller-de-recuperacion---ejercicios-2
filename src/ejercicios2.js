f1fc862180375db7ea7d8a11260496475978250b
Deivy38 <jhonny38camacho@gmail.com>
2026-03-12 21:03:10 -0500

    Ejercicio 9

--- src/ejercicios2.js
+++ src/ejercicios2.js
@@ -197,6 +197,31 @@ function obtenerEstacion(mes) {
 	// - Septiembre, Octubre, Noviembre → "Otoño"
 	// Si el mes no es válido, devuelve "Mes inválido"
 	// Tu código:
+	switch (mes) {
+
+	case 12:
+	case 1:
+	case 2:
+		return "Invierno";
+
+	case 3:
+	case 4:
+	case 5:
+		return "Primavera";
+
+	case 6:
+	case 7:
+	case 8:
+		return "Verano";
+
+	case 9:
+	case 10:
+	case 11:
+		return "Otoño";
+
+	default:
+		return "Mes inválido";
+	}
 }
 
 // Test 10:
